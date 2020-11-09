526560618ba76e540a844c0545c1bc017aaa82f8
Jeremy <jere.arde@gmail.com>
2020-11-08 19:41:58 -0600

    update build location, update tauri build and dev scripts

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -36,7 +36,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: 'public/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -46,7 +46,7 @@ export default {
 			// we'll extract any component CSS out into
 			// a separate file - better for performance
 			css: css => {
-				css.write('bundle.css');
+				css.write('public/bundle.css');
 			}
 		}),
 		// If you have external dependencies installed from
